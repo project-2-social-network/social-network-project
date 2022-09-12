fff9921b149c09a62d45ce2aaacea9350fc5bccc
Marina Laporte Boni <marinaolboni@gmail.com>
2022-09-12 20:44:20 +0200

    change port

--- app.js
+++ app.js
@@ -140,4 +140,6 @@ app.use((err, req, res, next) => {
   res.render("error", { err });
 });
 
-server.listen(3000, () => console.log("Listening on port 3000"));
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log("Listening on port 3000"));
